b348b43a51ebf568b6731017cd07439640400f34
Tamir Arnesty <tamir.arnesty@gmail.com>
2019-02-25 01:45:42 -0500

    Added social links and scroll to top button.

--- js/navigation.js
+++ js/navigation.js
@@ -14,14 +14,15 @@ function animateMenu(button) {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
+    document.getElementById("scrollButton").style.display = "block";
+    $("#socialLinks").animate({top: "20px"}, 100);
   } else {
-    document.getElementById("myBtn").style.display = "none";
+    document.getElementById("scrollButton").style.display = "none";
+    $("#socialLinks").animate({top: "50px"}, 100);
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  $("html, body").stop().animate({scrollTop:0});
 }
