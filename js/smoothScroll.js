6ce465b47b3336e725719436eca92b0256d6c5f9
Tamir Arnesty <tamir.arnesty@gmail.com>
2019-02-24 22:57:35 -0500

    Added scrollable sections with smooth scrolling on click.

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,5 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -16,7 +14,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 500, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
@@ -24,4 +22,3 @@ $(document).ready(function(){
     } // End if
   });
 });
-</script>
